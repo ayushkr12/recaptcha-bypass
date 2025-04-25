3ab1aea5d921e6b2b0c40ef843da0a7080c24e17
ayushkr12 <ayushkr91997@gmail.com>
2025-04-26 04:45:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     site_key = ""
-    return render_template('index.html', site_key=site_key)
+    return render_template('render.html', site_key=site_key)
 
 if __name__ == '__main__':
     app.run(debug=True, port=80)
